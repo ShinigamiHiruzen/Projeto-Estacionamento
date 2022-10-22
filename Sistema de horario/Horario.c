08e8945c02c67fcdc8413f6d4bab0301a6488feb
Eduardo Bomfim <eduardoandradebomfimjr@gmail.com>
2022-10-22 13:40:46 -0300

    second commit

---
+++ Sistema de horario/Horario.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+    int horas=0, minutos=0, segundos=0, horasatt, minutosatt, segundosatt, h, m, s, Hatt, Matt, Satt;
+
+    printf("Insira o horario de entrada do veiculo.\nHoras: ");
+    scanf("%i", &horas);
+    printf("Minutos: ");
+    scanf("%i", &minutos);
+    printf("Segundos: ");
+    scanf("%i", &segundos);
+
+    if (horas > horasatt && minutos > minutosatt && segundos > segundosatt){
+
+        horasatt = horas;
+        minutosatt = minutos;
+        segundosatt = segundos;
+    }
+     printf("Insira o horario de sai­da do veiculo.\nHoras: ");
+    scanf("%i", &horas);
+    printf("Minutos: ");
+    scanf("%i", &minutos);
+    printf("Segundos: ");
+    scanf("%i", &segundos);
+    
+    h = horasatt - horas;
+    m = minutosatt - minutos;
+    s = segundosatt - segundos;
+
+    if(h < 0){
+
+        Hatt = h * -1;
+    } if(m < 0){
+
+        Matt = m * -1;
+    }if(s < 0){
+
+        Satt = s * -1;
+    }
+
+    printf("O veiculo ficou no estacionamento por %.2i:%.2i:%.2i", Hatt, Matt, Satt);
+}
